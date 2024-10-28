5f4c52be05089487be9878a3a64b3008f2365288
Ilia Posov <ilia.posov@jetbrains.com>
2024-10-28 08:16:11 +0100

    make stool English

--- tasks/stool/stool.js
+++ tasks/stool/stool.js
@@ -72,13 +72,13 @@ export class Stool {
         return [
             {
                 name: "distance", //название параметра
-                title: "Расстояние до поверхности", //отображение названия для пользователя
+                title: "Surface distance", //отображение названия для пользователя
                 ordering: 'minimize', // 'maximize' - надо как можно больше, 'minimize' - как можно меньше
                 view: ""
             },
             {
                 name: "tiltAngle",
-                title: "Угол наклона",
+                title: "Inclination",
                 ordering: 'minimize',
                 view: "°"
             }
